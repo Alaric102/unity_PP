699f1e9edefd1eab2b774cdc2305c248f227e239
ildar <ildaralaric1998@gmail.com>
2022-02-11 14:16:16 +0300

    First commit

---
+++ Assets/Scripts/agentScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class agentScript : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
